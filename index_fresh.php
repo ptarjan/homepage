5ac9f716171b968bf74f8f64b8212796f5ca78ff
Paul Tarjan <github@paulisageek.com>
2012-01-02 22:51:32 -0800

    clean up CSS

--- index_fresh.php
+++ index_fresh.php
@@ -10,37 +10,6 @@
 <meta property="og:image" content="http://paultarjan.com/paul.jpg" />
 <meta property="fb:admins" content="218471" />
 <link rel="stylesheet" href="style.css" />
-<style>
-.photo {
-    width: 300px;
-    height: 300px;
-    float: left;
-    margin-right: 30px;
-    margin-bottom: 10px;
-}
-body {
-    width: 960px;
-    margin: 5px auto;
-}
-#menu {
-    text-align: center;
-}
-#menu a {
-    color: #000;
-    padding: 10px;
-    text-decoration: none;
-}
-#menu a:hover {
-    background: #e7e7e7;
-}
-h1.fn {
-    margin-top:0;
-    border:0;
-}
-h2 {
-    overflow: hidden;
-}
-</style>
 <body class="vcard">
 <div id="menu">
 <a href="mailto:web@paultarjan.com" class="email">Contact</a>
